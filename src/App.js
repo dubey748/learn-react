7c84e436c4ad082743b72f689bac8f614a2e7930
dubey748 <dubeyabhishek748@gmail.com>
2022-11-25 02:28:50 +0530

    Fourth

--- src/App.js
+++ src/App.js
@@ -2,29 +2,21 @@ import UpdateData from "./learn/UpdateData";
 import StateWithClass from "./learn/StateWithClass";
 import PropsWithFunctionalComponent from "./learn/PropsWithFunctionalComponent";
 import PropsInClassComponent from "./learn/PropsInClassComponent";
-import { Component } from "react";
-import React from "react";
+import GetInputBoxValue from "./learn/GetInputBoxValue";
 
-class App extends React.Component {
-constructor(){
-  super();
-  this.state={
-    name:"ABhishek"
-  }
-}
-
-  render() {
-    return (
-      <>
-        {/* <UpdateData /> */}
-        {/* <StateWithClass/> */}
-        {/*  <PropsWithFunctionalComponent name={"ABhishek"} email={"abc@"}/>
+function App() {
+  return (
+    <>
+      {/* <UpdateData /> */}
+      {/* <StateWithClass/> */}
+      {/*  <PropsWithFunctionalComponent name={"ABhishek"} email={"abc@"}/>
       <PropsWithFunctionalComponent name={"Himanshi"} email={"abc@"}/> */}
-        <PropsInClassComponent name={this.state.name} />
-        <button onClick={() => {this.setState({name:"dubey748"})}}>UpdateName</button>
-      </>
-    );
-  }
+      {/* <PropsInClassComponent name="ABhishek" />
+      <button onClick={()=>{}}>UpdateName</button> */}
+      <GetInputBoxValue/>
+
+    </>
+  );
 }
 
 export default App;
---
+++ src/learn/GetInputBoxValue.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function GetInputBoxValue() {
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+  function getData(val) {
+    setData(val.target.value);
+    setPrint(false) //hide data while typing
+  }
+  return (
+    <>
+      {/* <h1>Get Input Box Value, And entered data is, {data} </h1> */}
+      {print ? <h1>{data} </h1> : null}
+      <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}> Click me to print your Data</button>
+    </>
+  );
+}
+
+export default GetInputBoxValue;

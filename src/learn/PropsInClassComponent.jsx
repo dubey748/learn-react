949a1046060043864fe5b97ff4ea4583366ea439
ABhishek Dubey <65311526+dubey748@users.noreply.github.com>
2022-11-25 01:54:25 +0530

    Update PropsInClassComponent.jsx

--- src/learn/PropsInClassComponent.jsx
+++ src/learn/PropsInClassComponent.jsx
@@ -13,3 +13,27 @@ export default class PropsInClassComponent extends Component {
     );
   }
 }
+
+/* Write this in app.js 
+class App extends React.Component {
+constructor(){
+  super();
+  this.state={
+    name:"ABhishek"
+  }
+}
+
+  render() {
+    return (
+      <>
+        {/* <UpdateData /> */}
+        {/* <StateWithClass/> */}
+        {/*  <PropsWithFunctionalComponent name={"ABhishek"} email={"abc@"}/>
+      <PropsWithFunctionalComponent name={"Himanshi"} email={"abc@"}/> */}
+        <PropsInClassComponent name={this.state.name} />
+        <button onClick={() => {this.setState({name:"dubey748"})}}>UpdateName</button>
+      </>
+    );
+  }
+}
+*/
